5e888755163b2c2fe44b96f814da4748db21047c
Taras Kozodoi <acateely@gmail.com>
2022-12-20 18:02:06 +0200

    add components files

--- my-travel-journal/src/App.jsx
+++ my-travel-journal/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Header from "./components/Header"
+import Journal from './components/Journal'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <h1>My travel journal.</h1>
+      <Header/>
+      <Journal/>
     </div>
   )
 }
