28e90ca6401b8a9403c7c6fee307dcd0c960435a
Taras Kozodoi <acateely@gmail.com>
2022-12-21 12:35:02 +0200

    add title and date style

--- my-travel-journal/src/components/Journal.jsx
+++ my-travel-journal/src/components/Journal.jsx
@@ -11,7 +11,7 @@ export default function Journal(){
                     <a href="https://goo.gl/maps/PygTi2AYoq2L1GXz8">View on Google Maps</a>
                 </div>
                 <h1 className="journal-title">Mount Fuji</h1>
-                <p className="journal-date">12 Jan,2021 - 24 Jan,2021</p>
+                <p className="journal-date">12 Jan, 2021 - 24 Jan, 2021</p>
                 <p className="journal-description">
                     Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 
                     (12,380 feet). Mount Fuji is the single most popular tourist 
