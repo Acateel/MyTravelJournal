375623f9e7459d027cbb76684d3834ad4c937e3f
Taras Kozodoi <acateely@gmail.com>
2022-12-21 12:25:10 +0200

    add Journal example

--- my-travel-journal/src/components/Journal.jsx
+++ my-travel-journal/src/components/Journal.jsx
@@ -1,5 +1,23 @@
+import './Journal.css'
+
 export default function Journal(){
     return (
-        <h1>Journal</h1>
+        <section className="journal">
+            <img className="journal-image" src="../../public/images/mount-fuji.png" alt="Mount Fuji"/>
+            <div className="journal-text">
+                <div className="journal-location">
+                    <img src="../../public/images/map-point.png"/>
+                    <h4>JAPAN</h4>
+                    <a href="https://goo.gl/maps/PygTi2AYoq2L1GXz8">View on Google Maps</a>
+                </div>
+                <h1 className="journal-title">Mount Fuji</h1>
+                <p className="journal-date">12 Jan,2021 - 24 Jan,2021</p>
+                <p className="journal-description">
+                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 
+                    (12,380 feet). Mount Fuji is the single most popular tourist 
+                    site in Japan, for both Japanese and foreign tourists.
+                </p>
+            </div>
+        </section>
     )
 }
